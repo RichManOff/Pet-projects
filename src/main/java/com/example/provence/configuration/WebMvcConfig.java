ef3a13f03ee1fd41befc67f0f57001feec055e51
RichManOff <baimanovospan@gmail.com>
2024-04-16 13:09:01 +0500

    edited to take images fromm server files

--- src/main/java/com/example/provence/configuration/WebMvcConfig.java
+++ src/main/java/com/example/provence/configuration/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.example.provence.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -16,5 +17,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/provence/items/image/**")
+                .addResourceLocations("classpath:/static/img/menu_items/");
     }
 }
